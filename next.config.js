ccc4bbb635a982c31e3a687dfa18fa2c2705b6b6
kenta0313 <asanokenta@me.com>
2021-11-01 15:23:35 +0900

    ✨ create calender

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  optimizeFonts: false,
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
